3a7fc69bcf30f1c72688e091568da04ac7e026d9
Shubham <shubhamdhage000@gmail.com>
2022-02-19 03:56:43 +0530

    added some assets

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+// import * as dat from 'dat.gui'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-// Debug
-const gui = new dat.GUI()
+
+const gltfLoader = new GLTFLoader();
+
+
+
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -12,25 +16,57 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 
-// Materials
+// var fogColor = new THREE.Color(0x484848);
+ 
+// scene.background = fogColor;
+// scene.fog = new THREE.Fog(fogColor, 0.0025, 20);
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+
+//our object
+gltfLoader.load('finalModel.gltf', (gltf)=>{
+    gltf.scene.scale.set(0.4,0.4,0.4)
+    // gltf.scene.rotate.y =54;
+    scene.add(gltf.scene)
+})
+
+
+
+const intensity = 1
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const pointLight = new THREE.PointLight(0xffffff, intensity)  // x +ve
+pointLight.position.set(3,0,0)
 scene.add(pointLight)
+
+const pointLight1 = new THREE.PointLight(0xffffff, intensity) // x -ve
+pointLight1.position.set(-3,0,0)
+scene.add(pointLight1)
+
+const pointLight2 = new THREE.PointLight(0xffffff, intensity) //z +ve
+pointLight2.position.set(0,0,3)
+scene.add(pointLight2)
+
+const pointLight3 = new THREE.PointLight(0xffffff, intensity) //y +ve
+pointLight3.position.set(0,3,0)
+scene.add(pointLight3)
+
+
+const pointLight4 = new THREE.DirectionalLight(0xffffff, 5) //ambient on 1st quadrant
+scene.add(pointLight4)
+
+const pointLight5 = new THREE.DirectionalLight(0xffffff, 5) //ambient on 1st quadrant
+pointLight5.position.set(-3,3,3)
+scene.add(pointLight5)
+
+const pointLight6 = new THREE.DirectionalLight(0xffffff, 5) //ambient on 1st quadrant
+pointLight6.position.set(-3,-3,-3)
+scene.add(pointLight6)
+
+
+
 
 /**
  * Sizes
@@ -66,14 +102,15 @@ camera.position.z = 2
 scene.add(camera)
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha:true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -90,10 +127,10 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    camera.rotation.y = .5 * elapsedTime
 
     // Update Orbital Controls
-    // controls.update()
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
